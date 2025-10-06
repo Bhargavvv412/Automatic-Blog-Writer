d22f539235b3082b41a3349d77eb29ae3b31724c
Bhargavvv412 <bhargavvv412@gmail.com>
2025-10-06 23:50:24 +0530

    app.py

--- endpoint.py
+++ endpoint.py
@@ -14,3 +14,14 @@ def generate_blog_endpoint():
         return jsonify({"error":"Topic is required"}),400
     
     blog = genrate_blog(topic)
+
+    #Save to file
+    blog_filename= f"blogs/{topic.replace(' ',"_").lower()}.md"
+    with open(blog_filename,"w",encoding="utf-8") as f:
+        f.write(blog)
+
+    return jsonify({"message":"Blog generated successfully!","blog":blog})
+
+if __name__ == "__main__":
+    os.makedirs("blogs",exist_ok=True)
+    app.run(host="0.0.0.0",port=5000,debug=True)
